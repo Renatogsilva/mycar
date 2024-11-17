68b36c393047db9cae000e661b7704b0fcab0d2f
Renato Gomes <silvarenato180@gmail.com>
2024-11-17 17:55:27 -0300

    commit de criacao do projeto

---
+++ src/test/java/br/com/renatogsilva/my_car/MyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renatogsilva.my_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
