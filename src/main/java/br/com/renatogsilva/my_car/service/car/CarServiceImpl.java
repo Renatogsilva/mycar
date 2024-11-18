36a6d25a01e00a2df386b1d47ada2174b6715997
Renato Gomes <silvarenato180@gmail.com>
2024-11-17 21:08:44 -0300

    Ajustando e implementando os metodos do servico

--- src/main/java/br/com/renatogsilva/my_car/service/car/CarServiceImpl.java
+++ src/main/java/br/com/renatogsilva/my_car/service/car/CarServiceImpl.java
@@ -2,10 +2,13 @@ package br.com.renatogsilva.my_car.service.car;
 
 import br.com.renatogsilva.my_car.model.domain.Car;
 import br.com.renatogsilva.my_car.model.dto.CarDTO;
+import br.com.renatogsilva.my_car.model.enumerators.EnumStatus;
 import br.com.renatogsilva.my_car.repository.car.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,32 +19,60 @@ public class CarServiceImpl implements CarService{
 
     @Override
     public CarDTO create(CarDTO carDTO) {
+        Car car = new Car(carDTO);
+        car.setCreationDate(LocalDate.now());
+        car.setStatus(EnumStatus.ACTIVE);
 
-        return null;
+        this.carRepository.save(car);
+
+        return new CarDTO(car);
     }
 
     @Override
-    public CarDTO update(CarDTO carDTO) {
-        return null;
+    public CarDTO update(CarDTO carDTO, Long id) {
+        Car car = this.carRepository.findById(id).get();
+        car.setEngine(carDTO.getEngine());
+        car.setColor(carDTO.getColor());
+        car.setExchange(carDTO.getExchange());
+        car.setMark(carDTO.getMark());
+        car.setVersion(carDTO.getVersion());
+        car.setBodyStyle(carDTO.getBodyStyle());
+        car.setYearOfManufacture(carDTO.getYearOfManufacture());
+
+        this.carRepository.save(car);
+        return new CarDTO(car);
     }
 
     @Override
-    public CarDTO disable(CarDTO carDTO) {
-        return null;
+    public void disable(Long id) {
+        Car car = this.carRepository.findById(id).get();
+        car.setStatus(EnumStatus.INACTIVE);
+        car.setExclusionDate(LocalDate.now());
+        this.carRepository.save(car);
     }
 
     @Override
-    public CarDTO enable(CarDTO carDTO) {
-        return null;
+    public void enable(Long id) {
+        Car car = this.carRepository.findById(id).get();
+        car.setStatus(EnumStatus.ACTIVE);
+        car.setExclusionDate(null);
+        this.carRepository.save(car);
     }
 
     @Override
     public List<CarDTO> findAll() {
-        return List.of();
+        List<Car> cars = this.carRepository.findAll();
+        List<CarDTO> carDTOs = new ArrayList<>();
+        for (Car car : cars) {
+            carDTOs.add(new CarDTO(car));
+        }
+        return carDTOs;
     }
 
     @Override
     public CarDTO findById(Long id) {
-        return null;
+        Car car = this.carRepository.findById(id).get();
+
+        return new CarDTO(car);
     }
 }
