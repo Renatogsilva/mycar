f5724fc9d08ea2dad49e534f88a4b3c8499c0945
Renato Gomes <silvarenato180@gmail.com>
2024-11-17 21:07:51 -0300

    Implementando os endpoints da api

--- src/main/java/br/com/renatogsilva/my_car/controller/CarController.java
+++ src/main/java/br/com/renatogsilva/my_car/controller/CarController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/v1/car")
 @RequiredArgsConstructor
@@ -16,11 +18,36 @@ public class CarController {
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public CarDTO create(@RequestBody CarDTO carDTO) {
-        return carService.create(carDTO);
+        return this.carService.create(carDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CarDTO update(@PathVariable Long id, @RequestBody CarDTO carDTO) {
+        return this.carService.update(carDTO, id);
     }
 
     @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public CarDTO findById(@PathVariable Long id) {
         return this.carService.findById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CarDTO> findAll() {
+        return this.carService.findAll();
+    }
+
+    @DeleteMapping("/active/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        this.carService.enable(id);
+    }
+
+    @DeleteMapping("/desactive/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable(@PathVariable Long id) {
+        this.carService.disable(id);
     }
 }
