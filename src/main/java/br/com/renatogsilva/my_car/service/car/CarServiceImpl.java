a8ac2740cdbefad4b05987c18793ee1b8a98557d
Renato Gomes <silvarenato180@gmail.com>
2024-11-19 08:52:45 -0300

    Alterando implementacao da interface para receber um requestdto. Alterando implementacao da interface para enviar um responsedto. Adicionando anotacao transactional em cima dos metodos. Criando validacoes e lancamento de excecoes personalizadas.

--- src/main/java/br/com/renatogsilva/my_car/service/car/CarServiceImpl.java
+++ src/main/java/br/com/renatogsilva/my_car/service/car/CarServiceImpl.java
@@ -1,11 +1,17 @@
 package br.com.renatogsilva.my_car.service.car;
 
 import br.com.renatogsilva.my_car.model.domain.Car;
-import br.com.renatogsilva.my_car.model.dto.CarDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarRequestDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarResponseDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarResponseListDTO;
+import br.com.renatogsilva.my_car.model.enumerators.EnumMessageExceptions;
 import br.com.renatogsilva.my_car.model.enumerators.EnumStatus;
+import br.com.renatogsilva.my_car.model.exceptions.ObjectNotFoundException;
+import br.com.renatogsilva.my_car.model.validations.CarValidations;
 import br.com.renatogsilva.my_car.repository.car.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,66 +19,86 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class CarServiceImpl implements CarService{
+public class CarServiceImpl implements CarService {
 
     private final CarRepository carRepository;
+    private final CarValidations carValidations;
 
+    @Transactional
     @Override
-    public CarDTO create(CarDTO carDTO) {
-        Car car = new Car(carDTO);
+    public CarResponseDTO create(CarRequestDTO carRequestDTO) {
+        this.carValidations.checkRegistration(carRequestDTO);
+
+        Car car = new Car(carRequestDTO);
         car.setCreationDate(LocalDate.now());
-        car.setStatus(EnumStatus.ACTIVE);
+        car.setStatus(EnumStatus.ACTIVE.getCode());
 
         this.carRepository.save(car);
 
-        return new CarDTO(car);
+        return new CarResponseDTO(car);
     }
 
+    @Transactional
     @Override
-    public CarDTO update(CarDTO carDTO, Long id) {
-        Car car = this.carRepository.findById(id).get();
-        car.setEngine(carDTO.getEngine());
-        car.setColor(carDTO.getColor());
-        car.setExchange(carDTO.getExchange());
-        car.setMark(carDTO.getMark());
-        car.setVersion(carDTO.getVersion());
-        car.setBodyStyle(carDTO.getBodyStyle());
-        car.setYearOfManufacture(carDTO.getYearOfManufacture());
+    public CarResponseDTO update(CarRequestDTO carRequestDTO, Long id) {
+        Car car = this.carRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(EnumMessageExceptions.CAR_NOT_FOUND.getMessage(),
+                        EnumMessageExceptions.CAR_NOT_FOUND.getCode()));
+
+        car.setEngine(carRequestDTO.getEngine());
+        car.setColor(carRequestDTO.getColor());
+        car.setExchange(carRequestDTO.getExchange().getCode());
+        car.setMark(carRequestDTO.getMark());
+        car.setVersion(carRequestDTO.getVersion());
+        car.setBodyStyle(carRequestDTO.getBodyStyle());
+        car.setYearOfManufacture(carRequestDTO.getYearOfManufacture());
 
         this.carRepository.save(car);
-        return new CarDTO(car);
+        return new CarResponseDTO(car);
     }
 
+    @Transactional
     @Override
     public void disable(Long id) {
-        Car car = this.carRepository.findById(id).get();
-        car.setStatus(EnumStatus.INACTIVE);
+        Car car = this.carRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(EnumMessageExceptions.CAR_NOT_FOUND.getMessage(),
+                        EnumMessageExceptions.CAR_NOT_FOUND.getCode()));
+
+        car.setStatus(EnumStatus.INACTIVE.getCode());
         car.setExclusionDate(LocalDate.now());
         this.carRepository.save(car);
     }
 
+    @Transactional
     @Override
     public void enable(Long id) {
-        Car car = this.carRepository.findById(id).get();
-        car.setStatus(EnumStatus.ACTIVE);
+        Car car = this.carRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(EnumMessageExceptions.CAR_NOT_FOUND.getMessage(),
+                        EnumMessageExceptions.CAR_NOT_FOUND.getCode()));
+
+        car.setStatus(EnumStatus.ACTIVE.getCode());
         car.setExclusionDate(null);
         this.carRepository.save(car);
     }
 
+    @Transactional(readOnly = true)
     @Override
-    public List<CarDTO> findAll() {
+    public List<CarResponseListDTO> findAll() {
         List<Car> cars = this.carRepository.findAll();
-        List<CarDTO> carDTOs = new ArrayList<>();
+        List<CarResponseListDTO> carResponseListDTOS = new ArrayList<>();
         for (Car car : cars) {
-            carDTOs.add(new CarDTO(car));
+            carResponseListDTOS.add(new CarResponseListDTO(car));
         }
-        return carDTOs;
+        return carResponseListDTOS;
     }
 
+    @Transactional(readOnly = true)
     @Override
-    public CarDTO findById(Long id) {
-        Car car = this.carRepository.findById(id).get();
+    public CarResponseDTO findById(Long id) {
+        Car car = this.carRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(EnumMessageExceptions.CAR_NOT_FOUND.getMessage(),
+                        EnumMessageExceptions.CAR_NOT_FOUND.getCode()));
 
-        return new CarDTO(car);
+        return new CarResponseDTO(car);
     }
 }
