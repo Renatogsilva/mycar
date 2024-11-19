0686714b4be8fa533c98b3ca50b7fe71b6584c2b
Renato Gomes <silvarenato180@gmail.com>
2024-11-19 08:50:57 -0300

    Alterando interface para receber requestdto. Alterando interface para enviar responsedto.

--- src/main/java/br/com/renatogsilva/my_car/service/car/CarService.java
+++ src/main/java/br/com/renatogsilva/my_car/service/car/CarService.java
@@ -1,15 +1,17 @@
 package br.com.renatogsilva.my_car.service.car;
 
-import br.com.renatogsilva.my_car.model.dto.CarDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarRequestDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarResponseDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarResponseListDTO;
 
 import java.util.List;
 
 public interface CarService {
 
-    public CarDTO create(CarDTO carDTO);
-    public CarDTO update(CarDTO carDTO, Long id);
+    public CarResponseDTO create(CarRequestDTO carRequestDTO);
+    public CarResponseDTO update(CarRequestDTO carRequestDTO, Long id);
     public void disable(Long id);
     public void enable(Long id);
-    public List<CarDTO> findAll();
-    public CarDTO findById(Long id);
+    public List<CarResponseListDTO> findAll();
+    public CarResponseDTO findById(Long id);
 }
