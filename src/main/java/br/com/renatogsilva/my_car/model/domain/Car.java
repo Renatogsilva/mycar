cb3645ec3f149a832e9f5048564095c4131c8f80
Renato Gomes <silvarenato180@gmail.com>
2024-11-20 09:30:38 -0300

    Removendo import nao utilizado. Adicionando anotacao correta para validar enumstatus.

--- src/main/java/br/com/renatogsilva/my_car/model/domain/Car.java
+++ src/main/java/br/com/renatogsilva/my_car/model/domain/Car.java
@@ -2,7 +2,7 @@ package br.com.renatogsilva.my_car.model.domain;
 
 import br.com.renatogsilva.my_car.model.dto.car.CarRequestDTO;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.io.Serializable;
@@ -21,22 +21,29 @@ public class Car implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+
     @Column(length = 100, nullable = false)
     private String mark;
+
     @Column(name = "year_manufacture", nullable = false)
     private Integer yearOfManufacture;
+
     @Column(length = 50, nullable = false)
     private String color;
+
     @Column(length = 50, name = "body_style", nullable = false)
     private String bodyStyle;
+
     @Column(nullable = false)
     private Integer exchange;
+
     @Column(length = 20, nullable = false)
     private String engine;
+
     @Column(length = 20, nullable = false)
     private String version;
 
-    @NotBlank
+    @NotNull
     private Integer status;
 
     @Column(name = "creation_date")
