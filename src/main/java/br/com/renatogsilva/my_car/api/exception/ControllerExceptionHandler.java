da4959bdbf453ea7b3c275080534de48fb5fe60c
Renato Gomes <silvarenato180@gmail.com>
2024-11-20 09:44:08 -0300

    Adicionando as validacoes de excecoes de acordo com a demanda,

--- src/main/java/br/com/renatogsilva/my_car/api/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/renatogsilva/my_car/api/exception/ControllerExceptionHandler.java
@@ -1,8 +1,53 @@
 package br.com.renatogsilva.my_car.api.exception;
 
+import br.com.renatogsilva.my_car.model.enumerators.EnumMessageCarExceptions;
+import br.com.renatogsilva.my_car.model.enumerators.EnumMessageGenericExceptions;
+import br.com.renatogsilva.my_car.model.exceptions.car.CarDuplicationException;
+import br.com.renatogsilva.my_car.model.exceptions.car.CarNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CarDuplicationException.class)
+    public ResponseEntity<Object> handleCarDuplicationException(final CarDuplicationException ex, final WebRequest request) {
+        StandartError error = new StandartError(ex.getMessage(), ex.getCode(), LocalDateTime.now());
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(CarNotFoundException.class)
+    public ResponseEntity<Object> handleCarNotFoundException(final CarNotFoundException ex, final WebRequest request) {
+        StandartError error = new StandartError(ex.getMessage(), ex.getCode(), LocalDateTime.now());
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> methodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException ex,
+                                                                      final WebRequest request) {
+        StandartError error = new StandartError(EnumMessageGenericExceptions.INVALID_PARAMETER.getMessage(),
+                EnumMessageGenericExceptions.INVALID_PARAMETER.getCode(),
+                LocalDateTime.now());
+        return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        String message = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        StandartError error = new StandartError(message, status.value(), LocalDateTime.now());
+
+        return handleExceptionInternal(ex, error, headers, status, request);
+    }
 }
