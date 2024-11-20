9406415661d3df7d6ca83ad6f2e44dcca8713586
Renato Gomes <silvarenato180@gmail.com>
2024-11-20 10:42:28 -0300

    Alterando atributos do requestdto

--- src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserRequestDTO.java
+++ src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserRequestDTO.java
@@ -1,25 +1,21 @@
-package br.com.renatogsilva.my_car.model.dto;
+package br.com.renatogsilva.my_car.model.dto.user;
 
-import br.com.renatogsilva.my_car.model.enumerators.EnumSex;
+import br.com.renatogsilva.my_car.model.dto.person.PersonRequestDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Data
-public class UserDTO implements Serializable {
+public class UserRequestDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
     private String username;
-    private String email;
-    private String cpf;
-    private EnumSex sex;
-    private LocalDate birthday;
+    private PersonRequestDTO personRequestDTO;
 }
