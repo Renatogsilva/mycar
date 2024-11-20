6901c7820d81b4a557be46af881bd75756ca1740
Renato Gomes <silvarenato180@gmail.com>
2024-11-20 08:13:23 -0300

    Ajustes no enum de mensagens

--- src/main/java/br/com/renatogsilva/my_car/model/enumerators/EnumMessageCarExceptions.java
+++ src/main/java/br/com/renatogsilva/my_car/model/enumerators/EnumMessageCarExceptions.java
@@ -1,14 +1,14 @@
 package br.com.renatogsilva.my_car.model.enumerators;
 
-public enum EnumMessageExceptions {
+public enum EnumMessageCarExceptions {
     CAR_NOT_FOUND(404, "Veículo não encontrado"),
-    INVALID_PARAMETER(400, "Parâmetro inválido"),
+    INVALID_PARAMETER(400, "Parâmetro informado é inválido"),
     CAR_DUPLICATE(409, "Já existe um veículo cadastrado na base com essas caracteristicas");
 
     private Integer code;
     private String message;
 
-    private EnumMessageExceptions(Integer code, String message) {
+    private EnumMessageCarExceptions(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
@@ -21,12 +21,12 @@ public enum EnumMessageExceptions {
         return message;
     }
 
-    public static EnumMessageExceptions get(Integer cod) {
+    public static EnumMessageCarExceptions get(Integer cod) {
         if (cod == null) {
             return null;
         }
 
-        for (EnumMessageExceptions e : EnumMessageExceptions.values()) {
+        for (EnumMessageCarExceptions e : EnumMessageCarExceptions.values()) {
             if (e.code.equals(cod)) {
                 return e;
             }
