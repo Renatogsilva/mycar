455f9ecf979f2e2934938632e358de13dd6bb1e1
Renato Gomes <silvarenato180@gmail.com>
2024-11-22 08:32:44 -0300

    Implementando o dto de resposta em lista

--- src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserResponseListDTO.java
+++ src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserResponseListDTO.java
@@ -1,4 +1,23 @@
 package br.com.renatogsilva.my_car.model.dto.user;
 
-public class UserResponseListDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class UserResponseListDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String cpf;
+    private String enumSexDescription;
+    private String enumStatusDescription;
 }
