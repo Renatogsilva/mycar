b1f863296684f9bbe909b8f431f20de600f5f717
Renato Gomes <silvarenato180@gmail.com>
2024-11-22 08:36:52 -0300

    Criacao das classes de respostadto.

---
+++ src/main/java/br/com/renatogsilva/my_car/model/dto/person/PersonResponseDTO.java
@@ -0,0 +1,28 @@
+package br.com.renatogsilva.my_car.model.dto.person;
+
+import br.com.renatogsilva.my_car.model.dto.phone.PhoneResponseDTO;
+import br.com.renatogsilva.my_car.model.enumerators.EnumSex;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class PersonResponseDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String cpf;
+    private EnumSex sex;
+    private LocalDate birthDate;
+    private List<PhoneResponseDTO> phonesResponseDTOs;
+}
---
+++ src/main/java/br/com/renatogsilva/my_car/model/dto/phone/PhoneResponseDTO.java
@@ -0,0 +1,22 @@
+package br.com.renatogsilva.my_car.model.dto.phone;
+
+import br.com.renatogsilva.my_car.model.enumerators.EnumTypePhone;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class PhoneResponseDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String number;
+    private EnumTypePhone typePhone;
+    private boolean isMain;
+}
