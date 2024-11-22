86f12e68e0438066e8699b0b4a866cbd43f734e1
Renato Gomes <silvarenato180@gmail.com>
2024-11-22 08:32:11 -0300

    Implementando o dto de resposta

--- src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserResponseDTO.java
+++ src/main/java/br/com/renatogsilva/my_car/model/dto/user/UserResponseDTO.java
@@ -1,4 +1,21 @@
 package br.com.renatogsilva.my_car.model.dto.user;
 
-public class UserResponseDTO {
+import br.com.renatogsilva.my_car.model.dto.person.PersonResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class UserResponseDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private PersonResponseDTO personResponseDTO;
 }
