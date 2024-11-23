e207645401dd696f20f862bc7cc4cd7bc1e11761
Renato Gomes <silvarenato180@gmail.com>
2024-11-22 21:45:37 -0300

    Adicionando tamanho de campos na tabela.

--- src/main/java/br/com/renatogsilva/my_car/model/domain/User.java
+++ src/main/java/br/com/renatogsilva/my_car/model/domain/User.java
@@ -21,12 +21,12 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Long userId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
 
     @Column(name = "creation_date", nullable = false)
@@ -35,12 +35,12 @@ public class User implements Serializable {
     @Column(name = "is_primary_access", nullable = false)
     private boolean isPrimaryAccess;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     @Enumerated(EnumType.STRING)
     private EnumStatus status;
 
     @OneToOne(optional = false, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "fk_person_id", nullable = false)
+    @JoinColumn(name = "fk_person_id", nullable = false, unique = true)
     @ToString.Exclude
     private Person person;
 }
