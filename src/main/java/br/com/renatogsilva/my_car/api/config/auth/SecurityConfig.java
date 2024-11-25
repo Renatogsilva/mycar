8ae421773f7dda1d29bdd6e0ab9a7346c7b2be63
Renato Gomes <silvarenato180@gmail.com>
2024-11-25 07:43:34 -0300

    Removendo classe para um novo pacote. Alterando ordem de implementacao no metodo securityFilterChain.

--- src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
@@ -1,4 +1,4 @@
-package br.com.renatogsilva.my_car.api.config;
+package br.com.renatogsilva.my_car.api.config.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,14 +16,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/api/v1/auth").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+        ;
 
         return http.build();
     }
