dfbafe9f1c0034c3e2b406dcefb893137ce7b8b8
Renato Gomes <silvarenato180@gmail.com>
2024-11-28 08:08:08 -0300

    Adicionando novas mensagens de excecao.

--- src/main/java/br/com/renatogsilva/my_car/model/enumerators/EnumMessageUserExceptions.java
+++ src/main/java/br/com/renatogsilva/my_car/model/enumerators/EnumMessageUserExceptions.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum EnumMessageUserExceptions {
     USER_NOT_FOUND(404, "Usuário não encontrado"),
     USER_DUPLICATE(409, "Usuário de acesso informado já existe"),
-    CREDENTIALS_INVALID(401, "Usuário ou senha inválidos"),;
+    CREDENTIALS_INVALID(401, "Usuário ou senha inválidos"),
+    CURRENT_PASSWORD_INVALID(400, "Senha atual informada é inválida"),
+    USERNAME_INVALID(400, "Login informado é inválido"),;
 
     private Integer code;
     private String message;
