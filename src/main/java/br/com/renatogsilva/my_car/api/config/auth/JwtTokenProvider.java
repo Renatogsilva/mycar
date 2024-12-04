8876a816abfbf17a2abc2e12b37fdfa3e2a7a955
Renato Gomes <silvarenato180@gmail.com>
2024-12-04 17:36:05 -0300

    Adicionando valores no arquivo properties.

--- src/main/java/br/com/renatogsilva/my_car/api/config/auth/JwtTokenProvider.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/auth/JwtTokenProvider.java
@@ -4,11 +4,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Service
+@Component
 public class JwtTokenProvider {
 
     @Value("${jwt.secret.key}")
