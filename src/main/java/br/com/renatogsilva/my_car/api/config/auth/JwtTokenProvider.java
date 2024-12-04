6f7addba2373db673757934018edecff8687e7f6
Renato Gomes <silvarenato180@gmail.com>
2024-12-04 17:31:15 -0300

    Alterando anotacao.

--- src/main/java/br/com/renatogsilva/my_car/api/config/auth/JwtTokenProvider.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/auth/JwtTokenProvider.java
@@ -4,10 +4,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Component
+@Service
 public class JwtTokenProvider {
 
     @Value("${jwt.secret.key}")
