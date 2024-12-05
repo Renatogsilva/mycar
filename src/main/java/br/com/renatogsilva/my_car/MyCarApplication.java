563000cb11618a69d141d2bf8f24cad0fb513287
Renato Gomes <silvarenato180@gmail.com>
2024-12-05 08:07:05 -0300

    Removendo verificacao de problema de variaveis de ambiente.

--- src/main/java/br/com/renatogsilva/my_car/MyCarApplication.java
+++ src/main/java/br/com/renatogsilva/my_car/MyCarApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyCarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyCarApplication.class, args);
-
-		System.out.println("PG_URL: " + System.getenv("PG_URL"));
-		System.out.println("PG_USERNAME: " + System.getenv("PG_USERNAME"));
-		System.out.println("PG_PASSWORD: " + System.getenv("PG_PASSWORD"));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarApplication.class, args);
+    }
 }
