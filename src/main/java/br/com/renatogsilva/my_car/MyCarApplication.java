33932f1e08d98bdf7e508f5a8469634aaea1cad5
Renato Gomes <silvarenato180@gmail.com>
2024-12-05 08:03:42 -0300

    Verificando o problema das variaveis de ambiente.

--- src/main/java/br/com/renatogsilva/my_car/MyCarApplication.java
+++ src/main/java/br/com/renatogsilva/my_car/MyCarApplication.java
@@ -8,5 +8,9 @@ public class MyCarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyCarApplication.class, args);
+
+		System.out.println("PG_URL: " + System.getenv("PG_URL"));
+		System.out.println("PG_USERNAME: " + System.getenv("PG_USERNAME"));
+		System.out.println("PG_PASSWORD: " + System.getenv("PG_PASSWORD"));
 	}
 }
