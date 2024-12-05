eb3bfbb11272925c773c30fa2e86e4c683d17a09
Renato Gomes <silvarenato180@gmail.com>
2024-12-05 08:00:36 -0300

    Verificando o problema das variaveis de ambiente.

--- src/main/java/br/com/renatogsilva/my_car/api/config/AdminUserConfig.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/AdminUserConfig.java
@@ -48,5 +48,9 @@ public class AdminUserConfig implements CommandLineRunner {
 
 //        this.personRepository.save(user.getPerson());
 //        this.userRepository.save(user);
+
+        System.out.println("PG_URL: " + System.getenv("PG_URL"));
+        System.out.println("PG_USERNAME: " + System.getenv("PG_USERNAME"));
+        System.out.println("PG_PASSWORD: " + System.getenv("PG_PASSWORD"));
     }
 }
