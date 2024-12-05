e9f756004dde6e5e7beb7a84b87dc71abdbef434
Renato Gomes <silvarenato180@gmail.com>
2024-12-05 07:00:16 -0300

    Adicionando metodo de configuracao do cors.

--- src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -52,6 +55,18 @@ public class SecurityConfig {
         };
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:5173"); // Endereço do Swagger UI
+        configuration.addAllowedOrigin("http://localhost:3000"); // Outro domínio permitido
+        configuration.addAllowedMethod("*"); // Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
+        configuration.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+        configuration.setAllowCredentials(true); // Permitir credenciais
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
