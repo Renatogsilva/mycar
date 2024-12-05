b1bf18c1fda4f5088eb238faa3e0f96bb8e3ee08
Renato Gomes <silvarenato180@gmail.com>
2024-12-05 06:51:29 -0300

    Removendo configuracao do cors.

--- src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
+++ src/main/java/br/com/renatogsilva/my_car/api/config/auth/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
         http
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/login",
                                 "/swagger-ui/**",
