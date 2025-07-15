39ce9fd8fbd8c9c5fe6ed0afe761004b76f91c9b
Renato Gomes <silvarenato180@gmail.com>
2025-07-14 23:12:39 -0300

    Criando metodos auxiliares para os testes da classe CarServiceTest

--- src/test/java/br/com/renatogsilva/my_car/utils/FactoryCar.java
+++ src/test/java/br/com/renatogsilva/my_car/utils/FactoryCar.java
@@ -1,9 +1,16 @@
 package br.com.renatogsilva.my_car.utils;
 
 import br.com.renatogsilva.my_car.model.domain.Car;
+import br.com.renatogsilva.my_car.model.domain.User;
 import br.com.renatogsilva.my_car.model.dto.car.CarRequestDTO;
 import br.com.renatogsilva.my_car.model.dto.car.CarResponseDTO;
+import br.com.renatogsilva.my_car.model.dto.car.CarResponseListDTO;
 import br.com.renatogsilva.my_car.model.enumerators.EnumExchange;
+import br.com.renatogsilva.my_car.model.enumerators.EnumStatus;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FactoryCar {
 
@@ -50,5 +57,81 @@ public class FactoryCar {
         car.setYearOfManufacture(2020);
 
         return car;
+    }
+
+    public static Car createValidCarObject(){
+        Car car = new Car();
+
+        car.setCarId(1L);
+        car.setEngine("1.6");
+        car.setExchange(EnumExchange.MANUAL);
+        car.setColor("Branco");
+        car.setExclusionDate(null);
+        car.setMark("Wolkswagem");
+        car.setBodyStyle("Hatch");
+        car.setVersion("Polo MSI");
+        car.setYearOfManufacture(2020);
+        car.setStatus(EnumStatus.ACTIVE);
+        car.setCreationDate(LocalDate.now());
+        car.setUserCreation(new User(1L, null, null, null, false, null, null, null));
+
+        return car;
+    }
+
+    public static Car createValidCarObjectAndInactive(){
+        Car car = new Car();
+
+        car.setCarId(1L);
+        car.setEngine("1.6");
+        car.setExchange(EnumExchange.MANUAL);
+        car.setColor("Branco");
+        car.setExclusionDate(null);
+        car.setMark("Wolkswagem");
+        car.setBodyStyle("Hatch");
+        car.setVersion("Polo MSI");
+        car.setYearOfManufacture(2020);
+        car.setStatus(EnumStatus.INACTIVE);
+        car.setCreationDate(LocalDate.now());
+        car.setUserCreation(new User(1L, null, null, null, false, null, null, null));
+        car.setExclusionDate(LocalDate.now());
+        car.setUserExclusion(new User(2L, null, null, null, false, null, null, null));
+        return car;
+    }
+
+    public static List<Car> createListValidCarObject(){
+        List<Car> cars = new ArrayList<>();
+        Car car = new Car();
+
+        car.setCarId(1L);
+        car.setEngine("1.6");
+        car.setExchange(EnumExchange.MANUAL);
+        car.setColor("Branco");
+        car.setExclusionDate(null);
+        car.setMark("Wolkswagem");
+        car.setBodyStyle("Hatch");
+        car.setVersion("Polo MSI");
+        car.setYearOfManufacture(2020);
+        car.setStatus(EnumStatus.ACTIVE);
+        car.setCreationDate(LocalDate.now());
+        car.setUserCreation(new User(1L, null, null, null, false, null, null, null));
+
+        cars.add(car);
+
+        return cars;
+    }
+
+    public static List<CarResponseListDTO> carResponseListDTOList(){
+        List<CarResponseListDTO> carsResponseDto = new ArrayList<>();
+        CarResponseListDTO carResponseDTO = new CarResponseListDTO();
+
+        carResponseDTO.setMark("Wolkswagem");
+        carResponseDTO.setCarId(1L);
+        carResponseDTO.setEngine("1.6");
+        carResponseDTO.setVersion("Polo MSI");
+        carResponseDTO.setYearOfManufacture(2020);
+
+        carsResponseDto.add(carResponseDTO);
+
+        return carsResponseDto;
     }
 }
